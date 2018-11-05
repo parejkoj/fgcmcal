070407ac16683914fb09636afdaf8eeb72694016
Eli Rykoff <erykoff@stanford.edu>
2018-11-05 15:07:10 -0800

    Move matplotlib backend setting prior to any other import, where it belongs.

--- bin.src/fgcmFitCycle.py
+++ bin.src/fgcmFitCycle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # See COPYRIGHT file at the top of the source tree.
-from lsst.fgcmcal.fgcmFitCycle import FgcmFitCycleTask
-
 import matplotlib
 matplotlib.use("Agg")  # noqa
+
+from lsst.fgcmcal.fgcmFitCycle import FgcmFitCycleTask
 
 FgcmFitCycleTask.parseAndRun()
